4ac5830505849a9491d0241725866a20a87361c9
Thanat <bztzn512@gmail.com>
2021-07-15 19:15:36 -0400

    completed

--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import Button from "./Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
-
+import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState("");
   const [enteredUserAge, setEnteredUserAge] = useState("");
+  const [error, setError] = useState();
 
   const changeUserNameHandler = (event) => {
     setEnteredUserName(event.target.value);
@@ -15,16 +16,28 @@ const AddUser = (props) => {
     setEnteredUserAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   const addUserHandler = (event) => {
     event.preventDefault();
     if (
       enteredUserName.trim().lenth === 0 ||
       enteredUserAge.trim().lenth === 0
     ) {
+      setError({
+        title: "Invalid username",
+        message: "Please enter a valid usename",
+      });
       return;
     }
     if (+enteredUserAge < 1) {
       /* this one is return a string not a number we should add a plus sign*/
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid number",
+      });
       return;
     }
     props.onAddUser(enteredUserName, enteredUserAge);
@@ -33,25 +46,34 @@ const AddUser = (props) => {
   };
 
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">User Name</label>
-        <input
-          id="username"
-          type="text"
-          value={enteredUserName || ""}
-          onChange={changeUserNameHandler}
+    <div>
+      {error && (
+        <ErrorModal
+          title="An error Occured!"
+          message="please enter a vaild number or username"
+          onConfirm={errorHandler}
         />
-        <label htmlFor="">Add Age</label>
-        <input
-          id="age"
-          type="number"
-          value={enteredUserAge || ""}
-          onChange={changeAgeHandler}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">User Name</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUserName || ""}
+            onChange={changeUserNameHandler}
+          />
+          <label htmlFor="">Add Age</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredUserAge || ""}
+            onChange={changeAgeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
--- src/Components/User/Button.js
+++ src/Components/User/Button.js
@@ -4,7 +4,9 @@ import classes from "./Button.module.css";
 const Button = (props) => {
   return (
     <div>
-      <button className={classes.button}>Add User</button>
+      <button className={classes.button} type={props.type || "button"}>
+        {props.children}
+      </button>
     </div>
   );
 };
